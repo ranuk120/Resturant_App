43325e469f2f4991645d219e0a2b50379714f430
Osanda Bandara <ranukmalshan@gmail.com>
2022-11-09 00:34:54 +0530

    initial font % icon

--- src/Sample.js
+++ src/Sample.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const Sample = () => {
-  return <Text>Hello, I am your cat!</Text>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <MaterialIcons name="home" size={30} color="red" />
+      <Text style={{fontFamily: 'Heebo-Medium', fontSize: 30}}> Home </Text>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Sample;
